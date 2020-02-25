efddb78db500e8abe19b8b37845081e194d0face
Francesco Venica <francesco.venica@gmail.com>
2020-02-25 22:52:10 +0000

    added test for language

--- __tests__/fulltext.test.js
+++ __tests__/fulltext.test.js
@@ -141,6 +141,84 @@ test(
 );
 
 test(
+  'fulltext search field is created and working with language',
+  withSchema({
+    setup: `
+      create table fulltext_test.job (
+        id serial primary key,
+        name text not null,
+        full_text tsvector,
+        other_full_text tsvector
+      );
+      insert into fulltext_test.job (name, full_text, other_full_text) values 
+        ('test', to_tsvector('italian', 'apple fruit'), to_tsvector('italian', 'vegetable potato')), 
+        ('test 2', to_tsvector('italian', 'banana fruit'), to_tsvector('italian', 'vegetable pumpkin'));
+    `,
+    test: async ({ schema, pgClient }) => {
+      const query = `
+        query {
+          allJobs(
+            filter: {
+              fullText: {
+                matches: { language: "italian", value: "fruit" }
+              }
+              otherFullText: {
+                matches: { language: "italian", value: "vegetable" }
+              }
+            }
+            orderBy: [
+              FULL_TEXT_RANK_ASC
+              OTHER_FULL_TEXT_DESC
+            ]
+          ) {
+            nodes {
+              id
+              name
+              fullTextRank
+              otherFullTextRank
+            }
+          }
+        }
+      `;
+      expect(schema).toMatchSnapshot();
+
+      const result = await graphql(schema, query, null, { pgClient });
+      expect(result).not.toHaveProperty('errors');
+
+      const data = result.data.allJobs.nodes;
+      expect(data).toHaveLength(2);
+      data.map(n => expect(n.fullTextRank).not.toBeNull());
+      data.map(n => expect(n.otherFullTextRank).not.toBeNull());
+
+      const potatoQuery = `
+        query {
+          allJobs(
+            filter: {
+              otherFullText: {
+                matches: { language: "italian", value: "potato" }
+              }
+            }
+          ) {
+            nodes {
+              id
+              name
+              fullTextRank
+              otherFullTextRank
+            }
+          }
+        }
+      `;
+      const potatoResult = await graphql(schema, potatoQuery, null, { pgClient });
+      expect(potatoResult).not.toHaveProperty('errors');
+
+      const potatoData = potatoResult.data.allJobs.nodes;
+      expect(potatoData).toHaveLength(1);
+      potatoData.map(n => expect(n.fullTextRank).toBeNull());
+      potatoData.map(n => expect(n.otherFullTextRank).not.toBeNull());
+    },
+  }),
+);
+test(
   'fulltext search field is created',
   withSchema({
     setup: `
@@ -348,7 +426,7 @@ test(
         comment text,
         tsv tsvector
       );
-      
+
       insert into fulltext_test.clients (id, comment, tsv) values
         (1, 'Client A', tsvector('fruit apple')),
         (2, 'Client Z', tsvector('fruit avocado'));
